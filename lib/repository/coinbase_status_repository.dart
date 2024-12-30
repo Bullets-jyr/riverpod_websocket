3adf52c872f33462a6225328129a758a9ce1c57a
Bullets-jyr <jy.ryu.jiyoung@gmail.com>
2024-12-30 15:54:12 +0900

    feat: Status Repository Provider

--- lib/repository/coinbase_status_repository.dart
+++ lib/repository/coinbase_status_repository.dart
@@ -6,33 +6,36 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_websocket/api/coinbase_websocket.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-// final coinbaseStatusRepositoryProvider = AutoDisposeStreamProvider<Map<String, dynamic>>((ref) {
-//   final coinbaseWebsocket = ref.watch(coinbaseWebsocketProvider);
-//   final coinbaseStatusRepository = CoinbaseStatusRepository(coinbaseWebsocket);
-//
-//   ref.onDispose(() {
-//     coinbaseStatusRepository._unSubscribeToStatus();
-//     coinbaseStatusRepository.dispose();
-//     ref.invalidate(coinbaseWebsocketProvider);
-//
-//     debugPrint('CoinbaseStatusRepository disposed');
-//   });
-//   ref.onCancel(() {
-//     debugPrint('CoinbaseStatusRepository cancelled');
-//   });
-//   ref.onResume(() {
-//     debugPrint('CoinbaseStatusRepository resumed');
-//   });
-//   ref.onAddListener(() {
-//     debugPrint('CoinbaseStatusRepository added');
-//   });
-//   ref.onRemoveListener(() {
-//     debugPrint('CoinbaseStatusRepository removed');
-//   });
-//
-//
-//   return coinbaseStatusRepository.stream;
-// });
+// 이제 실제로 UI에서 이를 활용할 수 있습니다.
+// 실제로 이제 이 특정 스크림을 듣기 시작할 수 있습니다.
+final coinbaseStatusRepositoryProvider = AutoDisposeStreamProvider<Map<String, dynamic>>((ref) {
+  final coinbaseWebsocket = ref.watch(coinbaseWebsocketProvider);
+  final coinbaseStatusRepository = CoinbaseStatusRepository(coinbaseWebsocket);
+
+  // ref.onDispose(() {
+  //   coinbaseStatusRepository._unSubscribeToStatus();
+  //   coinbaseStatusRepository.dispose();
+  //   ref.invalidate(coinbaseWebsocketProvider);
+  //
+  //   debugPrint('CoinbaseStatusRepository disposed');
+  // });
+  // ref.onCancel(() {
+  //   debugPrint('CoinbaseStatusRepository cancelled');
+  // });
+  // ref.onResume(() {
+  //   debugPrint('CoinbaseStatusRepository resumed');
+  // });
+  // ref.onAddListener(() {
+  //   debugPrint('CoinbaseStatusRepository added');
+  // });
+  // ref.onRemoveListener(() {
+  //   debugPrint('CoinbaseStatusRepository removed');
+  // });
+
+  // 따라서 실제로 여기에서 반환 유형을 반환해야 합니다. 스트림이어야 합니다.
+  // 그럼 여기서 우리가 할 일은 여기서 스크림을 반환하는 것입니다.
+  return coinbaseStatusRepository.stream;
+});
 
 class CoinbaseStatusRepository {
   final CoinbaseWebsocket _coinbaseWebsocket;
@@ -46,7 +49,7 @@ class CoinbaseStatusRepository {
   bool _isSubscribed = false;
 
   final StreamController<Map<String, dynamic>> _streamController = StreamController<Map<String, dynamic>>();
-  // Stream<Map<String, dynamic>> get stream => _streamController.stream;
+  Stream<Map<String, dynamic>> get stream => _streamController.stream;
 
   void _init() {
     // 따라서 일단 연결하면 해당 상태의 이 특정 채널을 구독하게 됩니다.
